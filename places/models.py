39649030dbf2a32b91625ee5271bf2198270d097
Margarita Popova <margarita.kistanova@gmail.com>
2021-01-26 20:42:00 +0600

    Create places app

---
+++ places/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+print('models.py')
--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'places',
+    'places.apps.PlacesConfig',
 ]
 
 MIDDLEWARE = [
@@ -96,7 +96,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
