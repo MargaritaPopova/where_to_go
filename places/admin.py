b2dd5ecc1a43cc4e7efb7ded198a8e8f4f78ac78
Margarita Popova <margarita.kistanova@gmail.com>
2021-02-07 20:30:13 +0600

    verbose_name for all model fields admin inlines for images in location

--- places/admin.py
+++ places/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Location, Image
 
 
-admin.site.register(Location)
-admin.site.register(Image)
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+
+class LocationAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Координаты', {'fields': ['lng', 'lat']}),
+        ('Описание', {'fields': ['description_short', 'description_long']}),
+        ('Идентификаторы', {'fields': ['properties_placeId', 'properties_title']}),
+    ]
+    inlines = [ImageInline]
+
+
+admin.site.register(Location, LocationAdmin)
--- places/models.py
+++ places/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Location(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название для боковой панели')
-    description_short = models.TextField(max_length=1000)
-    description_long = models.TextField()
-    lng = models.CharField(max_length=30)
-    lat = models.CharField(max_length=30)
-    properties_title = models.CharField(max_length=200, default='', verbose_name='Название для точки')
+    description_short = models.TextField(max_length=1000, verbose_name='Краткое описание')
+    description_long = models.TextField(verbose_name='Полное описание')
+    lng = models.CharField(max_length=30, verbose_name='Долгота')
+    lat = models.CharField(max_length=30, verbose_name='Широта')
+    properties_title = models.CharField(max_length=200, default='', verbose_name='Название точки на карте')
     properties_placeId = models.CharField(max_length=200, default='', verbose_name='Уникальный идентификатор локации')
 
     def __str__(self):
@@ -15,8 +15,8 @@ class Location(models.Model):
 
 
 class Image(models.Model):
-    order_no = models.SmallIntegerField()
-    image = models.ImageField()
+    order_no = models.SmallIntegerField(verbose_name='Позиция')
+    image = models.ImageField(verbose_name='Файл')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
     def __str__(self):
