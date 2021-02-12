903a115d18b5d68e25ea01d53b1832d17eac6c38
Margarita Popova <margarita.kistanova@gmail.com>
2021-02-12 20:14:47 +0600

    urls_prod

--- where_to_go/settings_prod.py
+++ where_to_go/settings_prod.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'where_to_go.urls'
+ROOT_URLCONF = 'where_to_go.urls_prod'
 
 TEMPLATES = [
     {
---
+++ where_to_go/urls_prod.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('places.urls')),
+    path('tinymce/', include('tinymce.urls')),
+]
+
